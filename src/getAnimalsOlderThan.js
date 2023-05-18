932ab16d8119f97bf612c0c897114e2b282bdd2d
Axlvox <leandro.jlds@gmail.com>
2023-05-18 02:46:17 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const { species } = data;
+
+  const targetSpecies = species.find((bicho) => bicho.name === animal);
+
+  if (!targetSpecies) {
+    throw new Error('Espécie não encontrada');
+  }
+
+  return targetSpecies.residents.every((resident) => resident.age >= age);
 };
+
+const result = getAnimalsOlderThan('lions', 7);
+console.log(result);
 
 module.exports = getAnimalsOlderThan;
