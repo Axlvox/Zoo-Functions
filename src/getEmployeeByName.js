8e8379f9f5ca54fbd56509eab71da8273b3c28a9
Axlvox <leandro.jlds@gmail.com>
2023-05-18 04:55:32 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+const getEmployeeByName = (...employeeName) => {
+  const { employees: objeto } = data;
+  if (employeeName.length === 0) {
+    return {};
+  }
+
+  const worker = Object.values(objeto).find((employee) =>
+    employeeName.includes(employee.firstName)
+    || employeeName.includes(employee.lastName));
+
+  return worker || {};
 };
 
 module.exports = getEmployeeByName;
