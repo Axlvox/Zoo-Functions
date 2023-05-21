e809b2ee52efe1b6ff9fac6fb164594e49a6fd88
Axlvox <leandro.jlds@gmail.com>
2023-05-21 02:37:12 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+    test('retorna a quantidade de elefantes', () => {
+        expect(handlerElephants('count')).toBe(4);
+    });
+    test('retorna um array com a relação dos nomes de todos os elefantes', () => {
+        expect(handlerElephants('names')).toBe["Ilana", "Orval", "Bea", "Jefferson"];
+    });
+    test('retorna a média de idade dos elefantes', () => {
+        expect(handlerElephants('averageAge')).toBe(10.5);
+    });
+    test('retorna a localização dos elefantes dentro do Zoológico', () => {
+        const location = handlerElephants.location;
+        expect(handlerElephants('location')).toBe('NW');
+    });
+    test('retorna a popularidade dos elefantes', () => {
+        const popularity = handlerElephants.popularity;
+        expect(handlerElephants('popularity')).toBe(5);
+    });
+    test('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+        const availability = handlerElephants.availability;
+        expect(handlerElephants('availability')).toBe['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    });
+});
