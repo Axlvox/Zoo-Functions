272142b595bd62a49f65044f3a702aabecddf970
Axlvox <leandro.jlds@gmail.com>
2023-05-21 01:10:37 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const getAllAnimalCounts = (animalSpecies) => {
+  const animalCount = {};
+  animalSpecies.forEach((animal) => {
+    animalCount[animal.name] = animal.residents.length;
+  });
+  return animalCount;
+};
+
+const getSpeciesAnimalCount = (animalSpecies, species, sex) => {
+  const selectedSpecies = animalSpecies.find((animal) => animal.name === species);
+  if (selectedSpecies) {
+    if (sex) {
+      const selectedAnimals = selectedSpecies.residents.filter((resident) => resident.sex === sex);
+      return selectedAnimals.length;
+    }
+    return selectedSpecies.residents.length;
+  }
+  return {};
+};
+
+const getAllSpeciesAnimalCounts = (animalSpecies, sex) => {
+  const animalCount = {};
+  animalSpecies.forEach((animal) => {
+    if (sex) {
+      const selectedAnimals = animal.residents.filter((resident) => resident.sex === sex);
+      animalCount[animal.name] = selectedAnimals.length;
+    } else {
+      animalCount[animal.name] = animal.residents.length;
+    }
+  });
+  return animalCount;
+};
+
+const countAnimals = ({ species, sex } = {}) => {
+  const { species: animalSpecies } = data;
+
+  if (!species && !sex) {
+    return getAllAnimalCounts(animalSpecies);
+  }
+
+  if (species) {
+    return getSpeciesAnimalCount(animalSpecies, species, sex);
+  }
+
+  return getAllSpeciesAnimalCounts(animalSpecies, sex);
 };
 
 module.exports = countAnimals;
