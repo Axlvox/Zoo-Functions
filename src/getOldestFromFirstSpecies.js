b7676cbb5e063f8f5897f401b825c81ca2c7af40
Axlvox <leandro.jlds@gmail.com>
2023-05-22 04:06:27 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employee = data.employees.find((employee) => employee.id === id);
+
+  if (employee) {
+    const speciesId = employee.responsibleFor[0];
+    const species = data.species.find((specie) => specie.id === speciesId);
+
+    if (species) {
+      const oldestAnimal = species.residents.reduce((maisVelho, atual) => {
+        if (atual.age > maisVelho.age) {
+          return atual;
+        }
+        return maisVelho;
+      });
+
+      return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+    }
+  }
 };
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const { species: animalsDays } = data;
+  const dias = {
+    Monday: {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    },
+    Tuesday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+    Wednesday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+    Thursday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+    Friday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+    Saturday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+    Sunday: {
+      officeHour: '',
+      exhibition: [''],
+    },
+  }
+
+  const updatedDias = Object.keys(dias).map((day) => ({
+    ...dias[day],
+    officeHour: `${hours[day].open}am - ${hours[day].close}pm`,
+    exhibition: species.filter((animal) => animal.availability.includes(day)).map((animal) => animal.name),
+  }));
+
+  return updatedDias;
 };
 
 module.exports = getSchedule;
