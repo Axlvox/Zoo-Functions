c3bdc8a66b7a7b9b4ab703158973ec8032c30f26
Axlvox <leandro.jlds@gmail.com>
2023-05-21 23:53:14 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  // if (entrants === undefined || entrants.length === 0) {
+  //   return 0;
+  // }
+  
+  const conta = {
+    adult: 0,
+    senior: 0,
+    child: 0,
+  };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age >= 50) {
+      conta.senior++;
+    } else if (entrant.age >= 18) {
+      conta.adult++;
+    } else if (entrant.age < 18){
+      conta.child++;
+    }
+  });
+  return conta;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants|| entrants.length === 0) {
+    return 0;
+  }
+  const conta = countEntrants(entrants);
+  const prices = data.prices;
+
+  const totalCost = conta.adult * prices.adult + conta.senior * prices.senior + conta.child * prices.child;
+
+  return totalCost;
 };
 
 module.exports = { calculateEntry, countEntrants };
