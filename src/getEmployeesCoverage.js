734aca32c8babd684f758a6d45e7dcbf400cba94
Axlvox <leandro.jlds@gmail.com>
2023-05-22 23:23:52 -0300

    Tentativa de fazer o Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,75 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeeSpecies = (employee) => {
+  const { species } = data;
+  const { responsibleFor } = employee;
+
+  return species
+    .filter((specie) => responsibleFor.includes(specie.id))
+    .map((specie) => specie.name);
 };
+
+const getSpeciesLocations = (species) => {
+  const allSpecies = data.species;
+  const locations = [];
+
+  species.forEach((specie) => {
+    const specieData = allSpecies.find((s) => s.name === specie);
+    if (specieData) {
+      locations.push(specieData.location);
+    }
+  });
+
+  return Array.from(new Set(locations));
+};
+
+const getEmployeeInfo = (employee) => {
+  const species = getEmployeeSpecies(employee);
+  const locations = getSpeciesLocations(species);
+
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species,
+    locations,
+  };
+};
+
+function getFilteredEmployees({ name, id }, employees) {
+  if (!name && !id) {
+    return employees;
+  }
+  let filteredEmployees = employees;
+  if (name !== undefined) {
+    const secondName = name.split(' ')[1];
+    filteredEmployees = employees.filter(
+      (employee) =>
+        employee.firstName === name
+        || employee.lastName === name
+        || employee.lastName === secondName,
+    );
+  }
+  if (id !== undefined) {
+    filteredEmployees = filteredEmployees.filter((employee) => employee.id === id);
+  }
+  return filteredEmployees;
+}
+
+function getEmployeesCoverage(options) {
+  const { employees } = data;
+  const filteredEmployees = getFilteredEmployees(options, employees);
+
+  if (filteredEmployees.length === 0) {
+    throw new Error('Informações inválidas');
+  }
+
+  const employeeInfo = filteredEmployees.map((employee) => getEmployeeInfo(employee));
+
+  if (employeeInfo.length === 1) {
+    return employeeInfo[0];
+  }
+
+  return employeeInfo;
+}
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,73 @@
-const data = require('../data/zoo_data');
+// const zooData = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
+// const getSchedule = (scheduleTarget) => {
+//     if (!scheduleTarget) {
+//         const schedule = {};
+//         for (const day in zooData) {
+//           schedule[day] = {
+//             exhibition: zooData.species[day].exhibition,
+//             officeHour: zooData.species[day].officeHour,
+//           };
+//         }
+//         return schedule;
+//       }
+//   if (
+//     !zooData.species.some((species) => species.name === scheduleTarget) &&
+//     !Object.keys(zooData.hours).includes(scheduleTarget)
+//   ) {
+//     const schedule = {};
+//     for (const day in zooData.hours) {
+//       const availableAnimals = zooData.species
+//         .filter((species) => species.availability.includes(day))
+//         .map((species) => species.name);
+//       schedule[day] = {
+//         officeHour: `Open from ${zooData.hours[day].open}am until ${zooData.hours[day].close}pm`,
+//         exhibition: availableAnimals,
+//       };
+//     }
+//     return schedule;
+//   }
 
-};
+//   if (!scheduleTarget || typeof scheduleTarget !== 'string') {
+//     const schedule = {};
+//     for (const day in zooData.hours) {
+//       const animals = zooData.species
+//         .filter((animal) => animal.availability.includes(day))
+//         .map((animal) => animal.name);
+//       schedule[day] = {
+//         officeHour: `Open from ${zooData.hours[day].open}am until ${zooData.hours[day].close}pm`,
+//         exhibition: animals,
+//       };
+//     }
+//     return schedule;
+//   }
 
-module.exports = getSchedule;
+//   if (zooData.species.find((animal) => animal.name === scheduleTarget)) {
+//     const animalData = zooData.species.find((animal) => animal.name === scheduleTarget);
+//     return animalData.availability;
+//   }
+
+//   if (zooData.hours.hasOwnProperty(scheduleTarget)) {
+//     if (scheduleTarget === 'Monday') {
+//       return {
+//         [scheduleTarget]: {
+//           exhibition: 'The zoo will be closed!',
+//           officeHour: 'CLOSED',
+//         },
+//       };
+//     }
+
+//     const schedule = {
+//       [scheduleTarget]: {
+//         officeHour: `Open from ${zooData.hours[scheduleTarget].open}am until ${zooData.hours[scheduleTarget].close}pm`,
+//         exhibition: zooData.species
+//           .filter((animal) => animal.availability.includes(scheduleTarget))
+//           .map((animal) => animal.name),
+//       },
+//     };
+
+//     return schedule;
+//   }
+// };
+
+// module.exports = getSchedule;
